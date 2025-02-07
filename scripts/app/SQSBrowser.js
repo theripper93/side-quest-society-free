67edb4918e40c9d46f3f0087dca85414cc27e54a
theripper93 <tsplab@gmail.com>
2025-02-07 17:47:00 +0100

    a

--- scripts/app/SQSBrowser.js
+++ scripts/app/SQSBrowser.js
@@ -1,5 +1,8 @@
 import { MODULE_ID } from "../main.js";
 import { HandlebarsApplication, mergeClone } from "../lib/utils.js";
+import {getSetting, setSetting} from "../settings.js";
+
+let questServerData = null;
 
 export class SQSBrowser extends HandlebarsApplication {
     constructor() {
@@ -32,14 +35,31 @@ export class SQSBrowser extends HandlebarsApplication {
     }
 
     async _prepareContext(options) {
+        if (!questServerData) {
+            questServerData = await this.#getQuestServerData();
+        }
         const data = {};
         const freeQuests = await game.packs.get("side-quest-society-free.sqs-free-adventures").getDocuments();
         const premiumQuests = await game.packs.get("side-quest-society-premium.sqs-premium-adventures").getDocuments();
+        const currentVotes = getSetting("votes");
         data.quests = freeQuests.concat(premiumQuests).sort((a, b) => a.name.localeCompare(b.name));
         data.quests.forEach(quest => {
-            quest.votes1 = Math.random() * 100;
+            const questId = quest.name.slugify({strict: true});
+            const serverData = questServerData[questId] ?? {};
+            const votes1 = serverData.one ?? 0;
+            const votes2 = serverData.two ?? 0;
+            const totalVotes = votes1 + votes2;
+            const percentage = Math.round((votes1 / totalVotes) * 100);
+            quest.votes1prc = percentage;
+            quest.votes2prc = 100 - percentage;
+            quest.votes1 = votes1;
+            quest.votes2 = votes2;
+            const voted1 = currentVotes[questId] === "one";
+            const voted2 = currentVotes[questId] === "two";
+            quest.voted1 = voted1;
+            quest.voted2 = voted2;
+            quest.canVote = !serverData.closed;
         });
-        console.log(data.quests);
         return { ...data };
     }
 
@@ -59,7 +79,17 @@ export class SQSBrowser extends HandlebarsApplication {
         if (event.currentTarget.id === "vote-two") return this.#vote(document, "two");
     }
 
-    #vote(document, vote) {
+    async #getQuestServerData() {
+        const res = await fetch("https://theripper93.com/api/sqs");
+        const resJson = await res.json();
+        if (resJson.error) return ui.notifications.error(resJson.error);
+        return resJson.data.reduce((acc, val) => {
+            acc[val.questId] = {one: val.one, two: val.two};
+            return acc;
+        }, {});
+    }
+
+    async #vote(document, vote) {
         const questId = document.name.slugify({strict: true});
         const currentVote = getSetting("votes")[questId];
         if(currentVote === vote) return;
@@ -67,10 +97,29 @@ export class SQSBrowser extends HandlebarsApplication {
         const requestData = {
             questId,
             vote,
-            switch: !newVote,
+            switchVote: !newVote,
         };
 
-        console.log("Sending vote request", requestData);
+        //Post to server
+        const res = await fetch("https://theripper93.com/api/sqs", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(requestData),
+        })
+        console.log(await res.json());
+        if (res.status !== 200) return ui.notifications.error(res.statusText);
+        const currentSetting = getSetting("votes");
+        currentSetting[questId] = vote;
+        setSetting("votes", currentSetting);
+
+        const cached = questServerData[questId];
+        if (cached) {
+            cached[vote] += 1;
+            const other = vote === "one" ? "two" : "one";
+            if(!newVote) cached[other] -= 1;
+        }
 
         this.render();
     }
